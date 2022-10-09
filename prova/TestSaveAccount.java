3bde0a371cbf4a5c61d6c60dd27ed8a966bfb15d
Carmine <carmine.coppola001@studenti.uniparthenope.it>
2022-10-09 17:31:35 +0200

    agg

--- prova/TestSaveAccount.java
+++
@@ -1,9 +0,0 @@
-public class TestSaveAccount{
-
-    public static void main(String[] args) {
-        
-        SaveAccount prova = new SaveAccount(50);
-
-        System.out.println("Interesse: " + prova.getBalance());
-    }
-}
